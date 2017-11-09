cf8bd274e2561fcf238138e3a9dc29520e3ea050
nicht <miho0_0@naver.com>
2017-11-10 08:56:08 +0900

    Modificated some author's informations.
    .

--- TestCode/N_FoxVc/FoxEngine/FoxCore/Fvcengine.py
+++ TestCode/N_FoxVc/FoxEngine/FoxCore/Fvcengine.py
@@ -1,6 +1,6 @@
 # _*_coding:utf-8 _*_
 """
-made by Nicht = tayaka = Lee joon sung,
+made by Nicht = Tayaka = Lee joon sung,
 South Korea. Seoul. Gangnam. gaepodong.
 
 contact admin = miho0_0@naver.com OR anonymous0korea0@gmail.com(youtube) OR miho03092@gmail.com(gmail)
