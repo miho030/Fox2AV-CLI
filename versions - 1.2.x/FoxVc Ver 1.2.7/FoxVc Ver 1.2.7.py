4240252ac51a333e0fcbc3e677279d147804f124
AOI <miho0_0@naver.com>
2024-05-20 13:45:44 +0900

    Update FoxVc Ver 1.2.7.py

--- versions - 1.2.x/FoxVc Ver 1.2.7/FoxVc Ver 1.2.7.py
+++ versions - 1.2.x/FoxVc Ver 1.2.7/FoxVc Ver 1.2.7.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Author : Repubic of Korea, Seoul, JungSan HS  31227 Lee Joon Sung
-# Author_Helper : Republic of Korea, KyungGido, Kim Min Seok
-# youtube : anonymous0korea0@gmail.com ;;;; tayaka
+# Author : github/miho030
 # Email : miho0_0@naver.com
 
 import os
